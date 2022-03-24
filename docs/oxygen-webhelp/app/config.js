9f631ef0a7b190a681de23e853d19daf8db2b6ee
AnnMey <93004061+AnnMey@users.noreply.github.com>
2022-03-24 21:09:02 +0100

    new transition

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220324203910';
+                args = '20220324210749';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
